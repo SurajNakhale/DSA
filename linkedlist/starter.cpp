1b44cd6810e4fe0a52405cca37a78d143016f083
suraj <surajnakhale2407@gmail.com>
2025-11-20 12:23:47 +0530

    deletion in LL

--- linkedlist/starter.cpp
+++ linkedlist/starter.cpp
@@ -42,25 +42,133 @@ int lengthofLL(Node* head){
 }
 
 
+Node* removehead(Node* head){
+    Node* temp = head;
+    temp = head->next;
+    delete head;// in java it is handeled by garbage collector but in c++ we have manually remove the space
+    return temp;
+}
+
+
+Node*  removeTail(Node* head ){
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* temp = head;
+
+    while (temp->next->next != nullptr){
+        temp = temp->next;
+    }
+
+    delete temp->next;
+    temp->next = nullptr;
+    
+return head;
+}
+
+Node* deleteKelement(Node* head, int k ){
+    if(head == NULL) return head;
+
+    if(k == 1){
+        Node* temp = head;
+        temp = head->next;
+        delete head;
+        return temp;
+    }
+    int cnt = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL){
+        cnt++;
+        if(cnt == k){
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+
+    }
+    return head;
+
+}
+
+
+Node* deleteByValue(Node* head, int val ){
+    if(head == NULL) return head;
+
+    if(head->data == val){
+        Node* temp = head;
+        temp = head->next;
+        delete head;
+        return temp;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp != NULL){
+        if(temp->data == val){
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+
+    }
+    return head;
+
+}
+
+
 int main(){
 
     vector<int> arr = {2, 5, 6, 3};
     Node* head = convertarrtoll(arr);
+    
+
+
+    
+    // head = deleteByValue(head, 2);
+    // Node* temp = head;
+    // while(temp){
+    //     cout<< temp->data << " ";
+    //     temp = temp->next;
+    // }
+    
+
+    // head = deleteKelement(head, 1);
+    // Node* temp = head;
+    // while(temp){
+    //     cout<< temp->data << " ";
+    //     temp = temp->next;
+    // }
+    
+    //remove tail    
+    // Node* temp = removeTail(head);
+
+    // while(temp){
+    //     cout<< temp->data << " ";
+    //     temp = temp->next;
+    // }
+
+
+    //remove first element
+    // head = removehead(head);
+    // cout<< head->data;
+
 
     //print length of LL
-    cout<< lengthofLL(head)<< "\n";
+    // cout<< lengthofLL(head)<< "\n";
 
     //travesrsal in LL
-    Node* temp = head;
-    while(temp){
-        cout<< temp->data << " ";
-        temp = temp->next;
-    }
+    // Node* temp = head;
+    // while(temp){
+    //     cout<< temp->data << " ";
+    //     temp = temp->next;
+    // }
 
-    cout<< "\n";
+    // cout<< "\n";
 
     //print data at head
-    cout<< head->data << " \n";
-    Node* y = new Node(arr[0]);
-    cout<< y->data;
+    // cout<< head->data << " \n";
+    // Node* y = new Node(arr[0]);
+    // cout<< y->data;
 }
